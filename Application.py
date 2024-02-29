8931bd35e557651124f0a8647afee7526044c13f
Bertrand-DB <bertranddbezerra@gmail.com>
2024-02-29 19:12:00 -0300

    Adicionado botões

--- Application.py
+++ Application.py
@@ -1,6 +1,11 @@
 from tkinter import *
 import tkinter as tk
 
+LARGURA_BT = 0.1
+ALTURA_BT = 0.14
+ESPACAMENTO_Y = 0.02
+X_BOTAO = 0.89
+
 class Application():
 
     def __init__(self):
@@ -27,21 +32,21 @@ class Application():
 
     def botoes(self):
         self.bt_limpar = Button(self.quadro_1, text='Limpar')
-        self.bt_limpar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
+        self.bt_limpar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*1 + ALTURA_BT*0, relwidth=LARGURA_BT, relheight=ALTURA_BT)
         
         self.bt_selecionar = Button(self.quadro_1, text='Selecionar')
-        self.bt_selecionar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
+        self.bt_selecionar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*2 + ALTURA_BT*1, relwidth=LARGURA_BT, relheight=ALTURA_BT)
         
         self.bt_buscar = Button(self.quadro_1, text='Buscar')
-        self.bt_buscar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
+        self.bt_buscar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*3 + ALTURA_BT*2, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
         self.bt_adicionar = Button(self.quadro_1, text='Adicionar')
-        self.bt_adicionar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
-
+        self.bt_adicionar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*4 + ALTURA_BT*3, relwidth=LARGURA_BT, relheight=ALTURA_BT)
+ 
         self.bt_editar = Button(self.quadro_1, text='Editar')
-        self.bt_editar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
+        self.bt_editar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*5 + ALTURA_BT*4, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
         self.bt_deletar = Button(self.quadro_1, text='Deletar')
-        self.bt_deletar.place(relx=0.1, rely=0.02, relwidth=0.07, relheight=0.09)
+        self.bt_deletar.place(relx=X_BOTAO, rely=ESPACAMENTO_Y*6 + ALTURA_BT*5, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
 Application()
