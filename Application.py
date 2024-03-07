45400b2fceb96f934eb6431fe25f23919ee8afb9
Bertrand <Bertranddbezerra@gmail.com>
2024-03-07 20:30:16 -0300

    Adição de constantes de nome de tabela Labels e Entrys responsivas com place() Corrigido: funcao conexao e objeto conexao.connector com o mesmo nome -> TypeError: 'MySQLConnection' object is not callable função Deletar usa Id para deletar (não testado)

--- Application.py
+++ Application.py
@@ -4,6 +4,27 @@ from tkinter import ttk
 from tkinter import messagebox
 import tkinter as tk
 
+#Nome da tabela e colunas
+TABELA = ["Funcionários", "Id_Funcionários", "Nome", "Cargo", "Salário"]
+
+#Dados de conexão
+HOST = "localhost"
+USER = "root"
+PASSWORD = "369MyRoot*"
+DATABASE = "Projeto-BD_I"
+
+#constantes de posição dos botões
+LARGURA_BT = 0.12
+ALTURA_BT = 0.14
+ESPACAMENTO_BT = 0.02
+X_BOTAO = 1 - LARGURA_BT - ESPACAMENTO_BT
+
+#constantes de posição das labels e entrys
+ESPACAMENTO_LB = 0.08
+LARGURA_LABEL = 0.18
+ALTURA_LABEL = 0.1
+X_LABEL = 0.02
+
 
 class funcoes():
     
@@ -14,13 +35,13 @@ class funcoes():
         self.func_Salario_Entry.delete(0,END)
 
     def conexao(self):
-        self.conexao = mysql.connector.connect(
-            host = "127.0.0.1",
-            user = "root",
-            password="746832fC",
-            database="teste"
+        self.conn = mysql.connector.connect(
+            host = HOST,
+            user = USER,
+            password= PASSWORD,
+            database= DATABASE
         )
-        self.cursor= self.conexao.cursor()
+        self.cursor= self.conn.cursor()
 
     def insert_func(self):
         self.func_Id = self.func_Id_Entry.get()
@@ -28,20 +49,20 @@ class funcoes():
         self.func_Cargo = self.func_Cargo_Entry.get()
         self.func_Salario = self.func_Salario_Entry.get()
         self.conexao()
-        self.consulta ="INSERT INTO cadastro (id,nome,cargo,salario)VALUES(%s,%s,%s,%s)"
+        self.consulta ="INSERT INTO "+TABELA[0]+" ("+TABELA[1]+","+TABELA[2]+","+TABELA[3]+","+TABELA[4]+")VALUES(%s,%s,%s,%s)"
         self.valores=(self.func_Id,self.func_Nome,self.func_Cargo,self.func_Salario)
         self.cursor.execute(self.consulta,self.valores)
-        self.conexao.commit()
-        self.conexao.close()
+        self.conn.commit()
+        self.conn.close()
 
     def delete_func(self):
-        self.func_delete = self.func_Nome_Entry.get()
+        self.func_delete = self.func_Id_Entry.get()
         self.conexao()
-        self.consulta = "DELETE FROM cadastro WHERE nome = %s "
+        self.consulta = "DELETE FROM "+TABELA[0]+" WHERE "+TABELA[1]+" = %s "
         self.valores = [self.func_delete]
         self.cursor.execute(self.consulta,self.valores)
-        self.conexao.commit()
-        self.conexao.close()
+        self.conn.commit()
+        self.conn.close()
 
 class Application(funcoes):
 
@@ -54,15 +75,12 @@ class Application(funcoes):
         self.lista_quadro_2()
         self.root.mainloop()
 
-        
-
-
     def tela(self):
         self.root.geometry("720x640")
         self.root.title("Gerenciador de Restaurante")
         self.root.configure(background='#005089')
-        self.root.resizable(False, False)
-        self.root.wm_maxsize(1188,792)
+        self.root.resizable(True, True)
+        self.root.wm_maxsize(1080,720)
         self.root.wm_minsize(640,480)
 
     def quadros(self):
@@ -74,71 +92,69 @@ class Application(funcoes):
 
     def botoes(self):
         self.bt_limpar = Button(self.quadro_1, text='Limpar', width=7,command=self.clear_entry)
-        self.bt_limpar.grid(row=3,column=3, columnspan=1,padx=10,pady=5)
+        self.bt_limpar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*1 + ALTURA_BT*0, relwidth=LARGURA_BT, relheight=ALTURA_BT)
         
         self.bt_selecionar = Button(self.quadro_1, text='Selecionar', width=7)
-        self.bt_selecionar.grid(row=4,column=3,columnspan=1,padx=10,pady=5)
+        self.bt_selecionar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*2 + ALTURA_BT*1, relwidth=LARGURA_BT, relheight=ALTURA_BT)
         
         self.bt_buscar = Button(self.quadro_1, text='Buscar', width=7)
-        self.bt_buscar.grid(row=5,column=3,columnspan=1,padx=10,pady=5)
+        self.bt_buscar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*3 + ALTURA_BT*2, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
         self.bt_adicionar = Button(self.quadro_1, background="#32CD32", text='Adicionar', width=7,command=self.insert_func)
-        self.bt_adicionar.grid(row=3,column=4,columnspan=1,padx=10,pady=5)
+        self.bt_adicionar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*4 + ALTURA_BT*3, relwidth=LARGURA_BT, relheight=ALTURA_BT)
         
- 
         self.bt_editar = Button(self.quadro_1, background="#FFA500", text='Editar', width=7)
-        self.bt_editar.grid(row=4,column=4,columnspan=1,padx=10,pady=5)
+        self.bt_editar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*5 + ALTURA_BT*4, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
         self.bt_deletar = Button(self.quadro_1, background="#FF0000", text='Deletar', width=7,command=self.delete_func)
-        self.bt_deletar.grid(row=5,column=4,columnspan=1,padx=10,pady=5)
+        self.bt_deletar.place(relx=X_BOTAO, rely=ESPACAMENTO_BT*6 + ALTURA_BT*5, relwidth=LARGURA_BT, relheight=ALTURA_BT)
 
     def label_entry(self):
         #           lABEL DO CADASTRO DE FUNCIONÁRIOS
-        self.func_Id_Label = Label(self.quadro_1,text="ID Funcionário",font=('Arial',12),bg="#feffff", fg="#403d3d")
-        self.func_Id_Label.grid(row=3,column=0,columnspan=1,padx=0,pady=5)
+        self.func_Id_Label = Label(self.quadro_1,text="ID_Funcionário",font=('Arial',12),bg="#feffff", fg="#403d3d")
+        self.func_Id_Label.place(relx=X_LABEL, rely=ESPACAMENTO_LB*1 + ALTURA_LABEL*0, relwidth=LARGURA_LABEL, relheight=ALTURA_LABEL)
 
         self.func_Nome_Label = Label(self.quadro_1,text="Nome",font=('Arial',12),bg="#feffff", fg="#403d3d")
-        self.func_Nome_Label.grid(row=4,column=0,columnspan=1,padx=0,pady=5)
+        self.func_Nome_Label.place(relx=X_LABEL, rely=ESPACAMENTO_LB*2 + ALTURA_LABEL*1, relwidth=LARGURA_LABEL, relheight=ALTURA_LABEL)
 
         self.func_Cargo_Label = Label(self.quadro_1,text="Cargo",font=('Arial',12),bg="#feffff", fg="#403d3d")
-        self.func_Cargo_Label.grid(row=5,column=0,columnspan=1,padx=0,pady=5)
+        self.func_Cargo_Label.place(relx=X_LABEL, rely=ESPACAMENTO_LB*3 + ALTURA_LABEL*2, relwidth=LARGURA_LABEL, relheight=ALTURA_LABEL)
 
         self.func_Salario_Label = Label(self.quadro_1,text="Salário",font=('Arial',12),bg="#feffff", fg="#403d3d")
-        self.func_Salario_Label.grid(row=6,column=0,columnspan=1,padx=0,pady=5)
+        self.func_Salario_Label.place(relx=X_LABEL, rely=ESPACAMENTO_LB*4 + ALTURA_LABEL*3, relwidth=LARGURA_LABEL, relheight=ALTURA_LABEL)
         
         
         #           ENTRY DOS FUNCIONÁRIOS
         self.func_Id_Entry = Entry(self.quadro_1, width=25, justify='left', font=("Arial", 12), highlightthickness=1, relief="solid")
-        self.func_Id_Entry.grid(row=3,column=1,columnspan=1,padx=0,pady=5)
+        self.func_Id_Entry.place(relx=X_LABEL+LARGURA_LABEL, rely=ESPACAMENTO_LB*1 + ALTURA_LABEL*0, relwidth=0.1, relheight=ALTURA_LABEL)
 
         self.func_Nome_Entry = Entry(self.quadro_1, width=25, justify='left', font=("Arial", 12), highlightthickness=1, relief="solid")
-        self.func_Nome_Entry.grid(row=4,column=1,columnspan=1,padx=0,pady=5)
+        self.func_Nome_Entry.place(relx=X_LABEL+LARGURA_LABEL, rely=ESPACAMENTO_LB*2 + ALTURA_LABEL*1, relwidth=0.6, relheight=ALTURA_LABEL)
 
         self.func_Cargo_Entry = Entry(self.quadro_1, width=25, justify='left', font=("Arial", 12), highlightthickness=1, relief="solid")
-        self.func_Cargo_Entry.grid(row=5,column=1,columnspan=1,padx=0,pady=5)
+        self.func_Cargo_Entry.place(relx=X_LABEL+LARGURA_LABEL, rely=ESPACAMENTO_LB*3 + ALTURA_LABEL*2, relwidth=0.4, relheight=ALTURA_LABEL)
 
         self.func_Salario_Entry = Entry(self.quadro_1, width=25, justify='left', font=("Arial", 12), highlightthickness=1, relief="solid")
-        self.func_Salario_Entry.grid(row=6,column=1,columnspan=1,padx=0,pady=5)
-
-    
+        self.func_Salario_Entry.place(relx=X_LABEL+LARGURA_LABEL, rely=ESPACAMENTO_LB*4 + ALTURA_LABEL*3, relwidth=0.4, relheight=ALTURA_LABEL)
+ 
     def lista_quadro_2(self):
         self.lista = ttk.Treeview(self.quadro_2,height=3,columns=("col1","col2","col3","col4"))
-        self.lista.heading("#0",text="")
-        self.lista.heading("#1",text="Id")
-        self.lista.heading("#2",text="Nome")
-        self.lista.heading("#3",text="Cargo")
-        self.lista.heading("#4",text="Salário")
+        #self.lista.heading("#0",text="")
+        self.lista.heading("#0",text="Id_Funcionário")
+        self.lista.heading("#1",text="Nome")
+        self.lista.heading("#2",text="Cargo")
+        self.lista.heading("#3",text="Salário")
 
-        self.lista.column("#0",width=1)
-        self.lista.column("#1",width=50)
-        self.lista.column("#2",width=200)
-        self.lista.column("#3",width=125)
-        self.lista.column("#4",width=125)
+        #self.lista.column("#0",width=1)
+        self.lista.column("#0",width=100)
+        self.lista.column("#1",width=240)
+        self.lista.column("#2",width=150)
+        self.lista.column("#3",width=150)
 
-        self.lista.place(relx=0.01,rely=0.1,relwidth=0.95,relheight=0.85)
+        self.lista.place(relx=0.0,rely=0.0,relwidth=0.97,relheight=1)
         self.bar_rol = Scrollbar(self.quadro_2,orient='vertical')
         self.lista.configure(yscroll=self.bar_rol.set)
-        self.bar_rol.place(relx=0.96,rely=0.1,relwidth=0.02,relheight=0.85)
+        self.bar_rol.place(relx=0.97,rely=0.0,relwidth=0.03,relheight=1)
         
 
 
