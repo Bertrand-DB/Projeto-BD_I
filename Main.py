737132131531d1adf15e0fda52486b7c00a2dab1
Rodrigo Araújo de Souza <86501488+RodrigoAraujo12@users.noreply.github.com>
2024-04-30 23:44:58 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,9 +3,13 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import tkinter as tk
-from Tabela import *
+from Tabela_funcionario import *
 from Login_bd import *
 from Navegacao import *
+from Tabela_pedidos import *
+from Tabela_prato_pedido import *
+from Tabela_cardapio import *
+from Tabela_cliente import *
 
 usuario = Login_bd()
 #nomestabela = list(usuario.schema_metadata.keys())
@@ -13,7 +17,6 @@ usuario = Login_bd()
 #Navegacao(usuario.connection_data, "cardápio", usuario.schema_metadata["cardápio"])
 
 
-Navegacao(usuario.connection_data, usuario.table_metadata, usuario.view_metadata)
+#Navegacao(usuario.connection_data, usuario.table_metadata, usuario.view_metadata)
 #Tabela(usuario.connection_data, nomestabela[0], usuario.schema_metadata[nomestabela[0]])
-
-
+Tabela_funcionario(usuario.connection_data, "funcionários", usuario.table_metadata["funcionários"])
